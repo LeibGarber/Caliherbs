f869af53d24c03ff33e98a0cce6fdcd5ddf133ab
steve garber <steve.l.garber@gmail.com>
2018-09-13 20:31:24 +0300

    installed parallax but it doesnt work yet

--- src/About.js
+++ src/About.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import ParallaxBanner from "react-scroll-parallax";
 class About extends Component {
   render() {
     return (
-      <div>
-        <section className="hero">
-          <div
-            className="background-image"
-            style={{ backgroundImage: "url()" }}
-          />
-          <h1>This is a blank about view component</h1>
-          <h3>Write soome other things here about this here thing.</h3>
-        </section>
-      </div>
+      <ParallaxBanner
+        className="hero"
+        layers={[
+          {
+            image:
+              "https://media.wired.com/photos/5932369bb8eb31692072e2ac/master/w_1800,c_limit/Marijuana_Farm_02.jpg",
+            amount: 0.1,
+            slowerScrollRate: false
+          },
+          {
+            image:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Jjfslw8XdrHeldbPyuo2yBVbVLlurgDiiWu0yUeivkcOBnmDKg",
+            amount: 0.2,
+            slowerScrollRate: false
+          }
+        ]}
+        style={{
+          height: "500px"
+        }}
+      >
+        <h1>Blow topshelf cause midgrade is bad for your health!</h1>
+      </ParallaxBanner>
     );
   }
 }
